441073d9dfdebce9cea4d3d0bcb5a49610975783
4hrue2kd83f <ccai2271@gmail.com>
2021-12-21 09:44:38 -0700

    Make variables local

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var cl;
-var cln;
-var cr;
-var crn;
-var minDiff = 1500;
-var decreaseBy = 50;
-var score = 0;
-var state = false;
-var moreCorrect = false;
-var lessCorrect = true;
-
 window.onload = function () {
+    var cl;
+    var cln;
+    var cr;
+    var crn;
+    var minDiff = 1500;
+    var decreaseBy = 50;
+    var score = 0;
+    var state = false;
+    var moreCorrect = false;
+    var lessCorrect = true;
+
     next = document.getElementById("next");
     more = document.getElementById("more");
     less = document.getElementById("less");
@@ -70,39 +70,40 @@ window.onload = function () {
         }
     }
 
-    again.onclick = function() {
+    again.onclick = function () {
         location.reload();
+    }
+
+    function setupQuizzes() {
+        cr = cl;
+        while (Math.abs(cr - cl) <= minDiff) {
+            cr = randInt(data.length);
+        }
+        crn = data[cr];
+        data.splice(cr, 1);
+        moreCorrect = (crn[1] >= cln[1]);
+        lessCorrect = (crn[1] <= cln[1]);
+    }
+
+    function refreshHtml() {
+        document.getElementById("quizl").innerHTML = cln[0];
+        let tl = cln[1];
+        document.getElementById("takesl").innerHTML = tl.toLocaleString("en-US") + " takes";
+        document.getElementById("quizr").innerHTML = crn[0];
+        if (state) {
+            let tr = crn[1];
+            document.getElementById("takesr").innerHTML = tr.toLocaleString("en-US") + " takes";
+        } else {
+            document.getElementById("takesr").innerHTML = "?";
+        }
+        document.getElementById("score").innerHTML = "Score: " + score;
+    }
+
+    function gameOver() {
+        more.style.display = "none";
+        less.style.display = "none";
+        again.style.display = "inline-block";
+        document.getElementById("gameover").innerHTML = "Game Over";
     }
 };
 
-function setupQuizzes() {
-    cr = cl;
-    while (Math.abs(cr - cl) <= minDiff) {
-        cr = randInt(data.length);
-    }
-    crn = data[cr];
-    data.splice(cr, 1);
-    moreCorrect = (crn[1] >= cln[1]);
-    lessCorrect = (crn[1] <= cln[1]);
-}
-
-function refreshHtml() {
-    document.getElementById("quizl").innerHTML = cln[0];
-    let tl = cln[1];
-    document.getElementById("takesl").innerHTML = tl.toLocaleString("en-US") + " takes";
-    document.getElementById("quizr").innerHTML = crn[0];
-    if (state) {
-        let tr = crn[1];
-        document.getElementById("takesr").innerHTML = tr.toLocaleString("en-US") + " takes";
-    } else {
-        document.getElementById("takesr").innerHTML = "?";
-    }
-    document.getElementById("score").innerHTML = "Score: " + score;
-}
-
-function gameOver() {
-    more.style.display = "none";
-    less.style.display = "none";
-    again.style.display = "inline-block";
-    document.getElementById("gameover").innerHTML = "Game Over";
-}
